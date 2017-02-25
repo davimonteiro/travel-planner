01b22ae6eac7f3386d26496d445bcfd58d776387
Davi Monteiro Barbosa <davimonteiro.ce@gmail.com>
2017-02-25 14:28:13 -0300

    Refactoring the domain classes.

--- hotel-reservation/src/main/java/br/uece/Hotel.java
+++ hotel-reservation/src/main/java/br/uece/Hotel.java
@@ -1,10 +1,11 @@
 package br.uece;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
-@Builder
+@Data @Builder @NoArgsConstructor @AllArgsConstructor
 public class Hotel {
 	
 	private Long id;
